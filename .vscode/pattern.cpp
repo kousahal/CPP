fd8ce1639c8428681b7b4070582b6332cb3b71d0
Koushal Yadav <137811402+kousahal@users.noreply.github.com>
2024-09-08 23:14:06 +0530

    Pattern.cpp

--- .vscode/pattern.cpp
+++ .vscode/pattern.cpp
@@ -3,22 +3,27 @@ using namespace std;
 int main(){
 
 int n = 3;
-// int m = 4;
-// // cin>>n;
-// // cin>>m;
-// // for HOLLOW RECTANGULAR PATTERN;
-// // for(int i=1; i<=n ; i++){
-// //     for(int j=1; j<=m ; j++){
-// //         if (i==1 || j==1 || i==n || j==m){
-// //            cout<<"*";
-// //         }
-// //         else{
-// //             cout<<" ";
-// //         }
+int m = 4;
+cin>>n;
+cin>>m;
+// for HOLLOW RECTANGULAR PATTERN;
+// ******
+// *    *
+// *    *
+// ******
         
-// //     }
-// //     cout<<endl;
-// // }
+for(int i=1; i<=n ; i++){
+    for(int j=1; j<=m ; j++){
+        if (i==1 || j==1 || i==n || j==m){
+           cout<<"*";
+        }
+        else{
+            cout<<" ";
+        }
+        
+    }
+    cout<<endl;
+}
 
 
 // //for TRIANGULAR PATTERN;
@@ -27,39 +32,40 @@ int n = 3;
 // // ***
 // // ****
 
-// for(int i=1; i<=n; i++){
-//     for(int j=1; j<=i; j++){
-//         cout<<"* ";
-//     }
-//     cout<<endl;
-// } 
+for(int i=1; i<=n; i++){
+    for(int j=1; j<=i; j++){
+        cout<<"* ";
+    }
+    cout<<endl;
+} 
 
 // // ****
 // // ***
 // // **
 // // *
 
-// for(int i=1; i<=n ; i++){
-//     for(int j=1; j<=n-i+1; j++){
-//         cout<<"* ";
-//     }
-//     cout<<endl;
-// }
-
+for(int i=1; i<=n ; i++){
+    for(int j=1; j<=n-i+1; j++){
+        cout<<"* ";
+    }
+    cout<<endl;
+}
+        
+// for PYRAMID PATTERN:
 // //      * 
 // //     ***
 // //    *****
 // //   *******
  
-//  for(int i=1; i<=n; i++){
-//     for(int j=1; j<=n-i; j++){
-//         cout<<" ";
-//     }
-//     for(int j =1; j<=2*i-1;j++){
-//         cout<<"*";
-//     }
-//     cout<<endl;
-//  }
+ for(int i=1; i<=n; i++){
+    for(int j=1; j<=n-i; j++){
+        cout<<" ";
+    }
+    for(int j =1; j<=2*i-1;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+ }
 
 //  *****
 //   ***
