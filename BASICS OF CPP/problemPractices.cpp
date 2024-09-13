22f58a893964fbaedcb2b0fe0231605942a7a3f4
kousahal <koushalydv99@gmail.com>
2024-09-13 21:22:13 +0530

    practice

--- BASICS OF CPP/problemPractices.cpp
+++ BASICS OF CPP/problemPractices.cpp
@@ -1,24 +1,36 @@
+// // #include <iostream>
+// // using namespace std;
+
+// // int main() {
+// //     int x = 5;
+// //     do{
+// //         cout<< x;
+// //         x--;
+// //     }while(x<0);
+// // }
 // #include <iostream>
 // using namespace std;
 
 // int main() {
-//     int x = 5;
-//     do{
-//         cout<< x;
-//         x--;
-//     }while(x<0);
-// }
-#include <iostream>
+// 	int n;
+// 	cin>>n;
+// 	int count = 0;
+// 	while(n != 0){
+// 	    n /= 10;
+// 	    count++;
+// 	}
+// 	cout<<count << "\n";
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main() {
-	int n;
-	cin>>n;
-	int count = 0;
-	while(n != 0){
-	    n /= 10;
-	    count++;
-	}
-	cout<<count << "\n";
-
+int main()
+{
+    string A, B, C, D, E, F, G, H, I;
+    cin >> A >> B;
+    cin >> C >> D >> E;
+    cin >> F >> G >> H >> I;
+    cout << A << " " << B << " " << C << " " << D << " " << E << " " << F << " " << G << " " << H << " " << I;
+    return 0;
 }
+
