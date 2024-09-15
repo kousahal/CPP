70861845ab82ec4d6c1bfda12c617d9828310676
kousahal <koushalydv99@gmail.com>
2024-09-15 17:15:18 +0530

    operators

--- BASICS OF CPP/problemPractices.cpp
+++ BASICS OF CPP/problemPractices.cpp
@@ -1,7 +1,7 @@
-// // #include <iostream>
-// // using namespace std;
+#include <iostream>
+using namespace std;
 
-// // int main() {
+int main() {
 // //     int x = 5;
 // //     do{
 // //         cout<< x;
@@ -20,17 +20,25 @@
 // 	    count++;
 // 	}
 // 	cout<<count << "\n";
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
 
-using namespace std;
+// using namespace std;
 
-int main()
-{
-    string A, B, C, D, E, F, G, H, I;
-    cin >> A >> B;
-    cin >> C >> D >> E;
-    cin >> F >> G >> H >> I;
-    cout << A << " " << B << " " << C << " " << D << " " << E << " " << F << " " << G << " " << H << " " << I;
+// int main()
+// {
+//     string A, B, C, D, E, F, G, H, I;
+//     cin >> A >> B;
+//     cin >> C >> D >> E;
+//     cin >> F >> G >> H >> I;
+//     cout << A << " " << B << " " << C << " " << D << " " << E << " " << F << " " << G << " " << H << " " << I;
+   
+//    int a = 48;
+//    a = a<<1;
+//    cout << a;1
+    int i;
+    while(i!=0){
+        
+    }
     return 0;
 }
 
