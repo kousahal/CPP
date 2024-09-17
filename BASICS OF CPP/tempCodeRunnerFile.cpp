ecf7632de0f817ce84a254ed180ef90c73848f5e
kousahal <koushalydv99@gmail.com>
2024-09-17 18:40:16 +0530

    practice

--- BASICS OF CPP/tempCodeRunnerFile.cpp
+++ BASICS OF CPP/tempCodeRunnerFile.cpp
@@ -1,8 +1,1 @@
-   //  int ans = INT_MAX; //for finding minimum value we take ans value maximum;and for finding maximum viceoversa
-
-    //  for(int i =0; i<5;i++){
-    //   if(arr[i]<ans)
-    //   ans = arr[i];
-    //  }
-
-    //  cout<<ans;
+out<<"The input number is present on the index = "<<return;
