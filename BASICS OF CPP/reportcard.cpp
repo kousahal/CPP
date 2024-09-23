8df46757188e96317202bd74a851ed040993b24a
kousahal <koushalydv99@gmail.com>
2024-09-23 20:49:56 +0530

    Report_card

--- BASICS OF CPP/reportcard.cpp
+++ BASICS OF CPP/reportcard.cpp
@@ -8,7 +8,7 @@ class Report_Card
     int Total_Marks;
 
 public:
-    void input()
+    void TakinInput()
     {
         cout << "Name: ";
         cin >> Name;
@@ -16,26 +16,54 @@ public:
         cin >> Math_Marks;
         cout << "Science: ";
         cin >> Science_Marks;
-         cout << "Your name = " << Name << endl;
+        cout << "Your name = " << Name << endl;
         cout << "Marks in Math = " << Math_Marks << endl;
         cout << "Marks in Math = " << Science_Marks << endl;
     };
-    void output()
+    void Calculate()
     {
-       
-        Total_Marks = Math_Marks + Science_Marks;
-        cout << "Your Total Marks are = ";
-        cout << Total_Marks;
-    };
-    void percentage{
-        
+
+        {
+            Total_Marks = Math_Marks + Science_Marks;
+            cout << "Your Total Marks are = ";
+            cout << Total_Marks << endl;
+        }
     }
-    
+    void percentage()
+    {
+        int pin;
+        cout << "Enter pin for percentage:";
+        cin >> pin;
+        if (pin == 123)
+
+            
+        {
+            int per = Total_Marks / 2;
+            if (per > 90)
+            {
+                cout << "A" << endl;
+            }
+            else if (per < 89)
+            {
+                cout << "B" << endl;
+            }
+        }
+        else
+        {
+            cout << "Do not Access any other's Percentage ";
+        }
+    }
 };
 
 int main()
 {
     Report_Card S1;
-    S1.input();
-    S1.output();
+    S1.TakinInput();
+    S1.Calculate();
+    S1.percentage();
+
+    Report_Card S2;
+    S2.TakinInput();
+    S2.Calculate();
+    S2.percentage();
 }
