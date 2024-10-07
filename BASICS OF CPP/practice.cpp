2c12979a108eea2811ed14993f09ef793db33357
kousahal <koushalydv99@gmail.com>
2024-10-07 22:25:55 +0530

    practice

--- BASICS OF CPP/practice.cpp
+++ BASICS OF CPP/practice.cpp
@@ -1,24 +1,55 @@
 #include <iostream>
 using namespace std;
 
-class vehicle{
-    public:
-    int n;
+// class vehicle{
+//     public:
+//     int n;
 
-    void car(){
-        cout<<"Enter the number : ";
-        cin>>n;
-        cout<<n;
+//     void car(){
+//         cout<<"Enter the number : ";
+//         cin>>n;
+//         cout<<n;
+//     }
+
+//         void displaycar( ){
+//         cout<<"this is the second copy: ";
+//     };
+//     };
+
+// int main(){
+//     vehicle obj;
+//     obj.car();
+//     obj.displaycar();
+//     // car(4);
+// }
+
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int p;
+    cin >> p;
+
+    for (int i = 2; i <= p; i++) {
+        int j;
+        for (j = 2; j < i; j++) {
+            if (i % j == 0) {
+                break; // If a divisor is found, exit the loop
+            }
+        }
+
+        // If the loop completes without breaking, j will be equal to i
+        if (j == i) {
+            cout << i << endl;
+        }
     }
 
-        void displaycar( ){
-        cout<<"this is the second copy: ";
-    };
-    };
-
-int main(){
-    vehicle obj;
-    obj.car();
-    obj.displaycar();
-    // car(4);
+    return 0;
 }
+
